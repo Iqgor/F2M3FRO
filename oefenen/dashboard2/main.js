af8651851e9c09ee079a207bcde449aae2f9b138
Igor <33993@ma-web.nl>
2022-03-29 13:06:53 +0200

    d2 update

--- oefenen/dashboard2/main.js
+++ oefenen/dashboard2/main.js
@@ -13,21 +13,32 @@ let pokemon = fetch( "https://pokeapi.co/api/v2/pokemon/" +randomNumber)
 
 const jaofnee = document.getElementById("js--jaofnee")
 
-let randomNumber1 = Math.floor(Math.random() * 4 + 1 );
-console.log(randomNumber1)
+let randomNumber1 = Math.floor(Math.random() * 2 + 0 );
+console.log(randomNumber1 )
 
 jaofnee.innerText = "Ga je hem pakken of niet "
 
 function vangButton(){
-    if (randomNumber1 <=+ 2){
+    if (randomNumber1 === 1){
         jaofnee.innerText = "Gepakt!"
     }
     else{
         jaofnee.innerText = "Gefaalt!"
     }
-
-
-    
 }
 
+
+
+const title = document.getElementById('js--showTitle')
+const be = document.getElementById("js--showBe")
+
+let show = fetch("https://api.tvmaze.com/search/shows?q=phineas%20and%20ferb")
+
+.then(function(response){
+    return response.json();
+})
+.then(function(realData){
+    title.innerText = realData[0].show.name
+    be.innerHTML = realData[1].show.summary	
+})
 
